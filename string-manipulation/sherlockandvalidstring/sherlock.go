442fef9dfc40c8ec892a9fddc7939fb4eec03e71
John Henderson <john.henderson@shareandcharge.com>
2020-08-23 09:06:26 -0700

    initial sherlock solution

---
+++ string-manipulation/sherlockandvalidstring/sherlock.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
+
+// Complete the isValid function below.
+func isValid(s string) string {
+	var letterCounts [26]int
+	for _, c := range s {
+		letter := int(c) - 97
+		letterCounts[letter]++
+	}
+
+	first := letterCounts[0]
+	differentCount := 0
+	for i := 0; i < len(letterCounts); i++ {
+		curCount := letterCounts[i]
+		if curCount == 0 {
+			continue
+		}
+		if curCount > first+1 || curCount < first-1 {
+			return "NO"
+		}
+		if curCount != first {
+			if differentCount == 0 {
+				differentCount++
+			} else {
+				return "NO"
+			}
+		}
+	}
+	return "YES"
+}
+
+func main() {
+	// reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	// stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	// checkError(err)
+
+	// defer stdout.Close()
+
+	// writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	// s := readLine(reader)
+	s := "ibfdgaeadiaefgbhbdghhhbgdfgeiccbiehhfcggchgghadhdhagfbahhddgghbdehidbibaeaagaeeigffcebfbaieggabcfbiiedcabfihchdfabifahcbhagccbdfifhghcadfiadeeaheeddddiecaicbgigccageicehfdhdgafaddhffadigfhhcaedcedecafeacbdacgfgfeeibgaiffdehigebhhehiaahfidibccdcdagifgaihacihadecgifihbebffebdfbchbgigeccahgihbcbcaggebaaafgfedbfgagfediddghdgbgehhhifhgcedechahidcbchebheihaadbbbiaiccededchdagfhccfdefigfibifabeiaccghcegfbcghaefifbachebaacbhbfgfddeceababbacgffbagidebeadfihaefefegbghgddbbgddeehgfbhafbccidebgehifafgbghafacgfdccgifdcbbbidfifhdaibgigebigaedeaaiadegfefbhacgddhchgcbgcaeaieiegiffchbgbebgbehbbfcebciiagacaiechdigbgbghefcahgbhfibhedaeeiffebdiabcifgccdefabccdghehfibfiifdaicfedagahhdcbhbicdgibgcedieihcichadgchgbdcdagaihebbabhibcihicadgadfcihdheefbhffiageddhgahaidfdhhdbgciiaciegchiiebfbcbhaeagccfhbfhaddagnfieihghfbaggiffbbfbecgaiiidccdceadbbdfgigibgcgchafccdchgifdeieicbaididhfcfdedbhaadedfageigfdehgcdaecaebebebfcieaecfagfdieaefdiedbcadchabhebgehiidfcgahcdhcdhgchhiiheffiifeegcfdgbdeffhgeghdfhbfbifgidcafbfcd"
+	result := isValid(s)
+	fmt.Println(result)
+
+	// fmt.Fprintf(writer, "%s\n", result)
+
+	// writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
